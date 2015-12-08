aa38841ec4c8ac0edcfda703bb1c6334c305485c
student1 <student1@jh-mbair-158-06.d214.org>
2015-12-08 09:58:27 -0600

    Placing the Noun from the UITextField into the Noun variable of class MadLib.

--- wordplayApp/ViewController.swift
+++ wordplayApp/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nounTexfField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    
+    var madLibInViewController = madLib()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-//--
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    @IBAction func nounOnButtonTap(sender: UIButton) {
+        madLibInViewController.noun = (nounTexfField.text!)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        <#code#>
+    }
+    
+    func nounToClassMadLib() {
+        
+    }
 }
 
